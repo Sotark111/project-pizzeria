4ffd4dcdbf61071d8d93b8c6ce84c05e2e678f33
Adam Wardak <adamwa12@interia.pl>
2025-06-27 16:20:45 +0200

    pizzeria

--- src/js/app.js
+++ src/js/app.js
@@ -1,81 +1,127 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import { Booking } from './components/Booking.js';
 
 
 
 
-  const app = {
-    init: function () {
-      const thisApp = this;
-      thisApp.initData();
-      thisApp.initCart(); 
-      
-    },
 
-    initCart: function () {
-  const thisApp = this;
-  thisApp.cart = new Cart(document.querySelector(select.containerOf.cart));
+ const app = {
+  initPages: function () {
+    const thisApp = this;
 
-  const orderForm = document.querySelector(select.cart.form);
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.navLinks);
 
-  thisApp.productList = document.querySelector(select.containerOf.menu);
-  thisApp.productList.addEventListener('add-to-cart', function(event){
-  thisApp.cart.add(event.detail.product);
-});
+    const idFromHash = window.location.hash.replace('#/', '');
 
-  if (orderForm) {
-    orderForm.addEventListener('submit', function (event) {
-      event.preventDefault(); 
+    let pageMatchingHash = thisApp.pages[0].id;
 
-      const phone = orderForm.querySelector(select.cart.phone).value;
-      const address = orderForm.querySelector(select.cart.address).value;
-
-      if (!phone || !address) {
-        alert('Uzupełnij numer telefonu i adres!');
-        return;
-      }
-
-      console.log('Zamówienie złożone!');
-      console.log('Telefon:', phone);
-      console.log('Adres:', address);
-
-      orderForm.reset();
-    });
-  }
-},
-
-    initData: function () {
-      const thisApp = this;
-      thisApp.data = {};
-
-      const url = settings.db.url + '/' + settings.db.products;
-      fetch(url)
-        .then(function (rawResponse) {
-          return rawResponse.json();
-        })
-        .then(function (parsedResponse) {
-          console.log('parsedResponse', parsedResponse);
-          thisApp.data.products = parsedResponse;
-          console.log('thisApp.data', JSON.stringify(thisApp.data));
-          thisApp.initMenu();
-        });
-
-      console.log('thisApp.data', JSON.stringify(thisApp.data));
-
-            },
-    
-   
- 
-
-
-    initMenu: function () {
-      const thisApp = this;
-      for (let productData of thisApp.data.products) {
-        new Product(productData.id, productData);
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
       }
     }
-    
-  };
-  
-  app.init();
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const id = this.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
+  initBooking: function () {
+    const thisApp = this;
+    const bookingContainer = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(bookingContainer);
+  },
+
+  init: function () {
+    const thisApp = this;
+    thisApp.initData();
+    thisApp.initCart();
+    thisApp.initPages();
+    thisApp.initBooking(); 
+  },
+
+  initCart: function () {
+    const thisApp = this;
+    thisApp.cart = new Cart(document.querySelector(select.containerOf.cart));
+
+    const orderForm = document.querySelector(select.cart.form);
+
+    thisApp.productList = document.querySelector(select.containerOf.menu);
+    thisApp.productList.addEventListener('add-to-cart', function (event) {
+      thisApp.cart.add(event.detail.product);
+    });
+
+    if (orderForm) {
+      orderForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const phone = orderForm.querySelector(select.cart.phone).value;
+        const address = orderForm.querySelector(select.cart.address).value;
+
+        if (!phone || !address) {
+          alert('Uzupełnij numer telefonu i adres!');
+          return;
+        }
+
+        console.log('Zamówienie złożone!');
+        console.log('Telefon:', phone);
+        console.log('Adres:', address);
+
+        orderForm.reset();
+      });
+    }
+  },
+
+  initData: function () {
+    const thisApp = this;
+    thisApp.data = {};
+
+    const url = settings.db.url + '/' + settings.db.products;
+    fetch(url)
+      .then(function (rawResponse) {
+        return rawResponse.json();
+      })
+      .then(function (parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+        thisApp.data.products = parsedResponse;
+        thisApp.initMenu();
+      });
+
+    console.log('thisApp.data', JSON.stringify(thisApp.data));
+  },
+
+  initMenu: function () {
+    const thisApp = this;
+    for (let productData of thisApp.data.products) {
+      new Product(productData.id, productData);
+    }
+  },
+};
+
+app.init();
